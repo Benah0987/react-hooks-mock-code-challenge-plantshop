98a6b204d5db0ab4270d12ffab5d71762fcf0938
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-09 09:34:28 +0300

    fetched data from db.json

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PlantCard from "./PlantCard";
 
+
 function PlantList() {
+  //creating a useState
+  const [plants, setPlants] = useState([])
+
+ //already imported the useEffect
+ useEffect (() => {
+   fetch("http://localhost:6001/plants")
+   .then((response) => response.json())
+   .then((data) => setPlants(data));
+ })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+
+    <ul className="cards">
+      {/*adding the props needed*/}
+      {plants.map((id,name, image, price) => (
+        <PlantCard 
+         key={id}
+         id ={id}
+         Image={image}
+         price ={price}
+      />
+      )
+      )}
+      </ul>
   );
 }
 
