af27a1bdbcb5bca786f0569136f2a5e9d9c601c5
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-09 13:08:45 +0300

    added a handler button

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
+///useState to handle the soldOut
 
 
 //passing props to ensure each card displays
 function PlantCard({name, image, price,soldOut}) {
+  
+  //statte soldOut, initial value soldOut
+  const [issoldOut, setisSoldOut] = useState(soldOut)
+
+  //when the button is clicked change state
+  const handleClick = () => {
+    setisSoldOut(!soldOut)
+  }
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {soldOut ? (
-        <button className="primary">In Stock</button>
+      {issoldOut ? (
+        <button className="primary" onClick = {handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick = {handleClick}>Out of Stock</button>
       )}
     </li>
   );
